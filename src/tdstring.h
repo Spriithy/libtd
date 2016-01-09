c4ce17c59002c47cdbebd425f121219f99245059
Spriithy <theo.dano@gmail.com>
2016-01-09 02:35:34 +0100

    space update

--- src/tdstring.h
+++ src/tdstring.h
@@ -38,34 +38,34 @@
 typedef unsigned long	size_t;
 
 /* memory management functions */
-void	*td_memset		(void *ptr, int c, size_t count);
-void	*td_bzero		(void *ptr, size_t count);
-void   	*td_memcpy		(void *dst, const void *src, size_t count);
-void	*td_memccpy		(void *dst, const void *src, int c, size_t count);
-void	*td_memmove		(void *dst, const void *src, size_t count);
-void	*td_memchr		(const void *ptr, int c, size_t count);
-int		 td_memcmp		(const void *s1, const void *s2, size_t count);
+void	*td_memset	(void *ptr, int c, size_t count);
+void	*td_bzero	(void *ptr, size_t count);
+void	*td_memcpy	(void *dst, const void *src, size_t count);
+void	*td_memccpy	(void *dst, const void *src, int c, size_t count);
+void	*td_memmove	(void *dst, const void *src, size_t count);
+void	*td_memchr	(const void *ptr, int c, size_t count);
+int		td_memcmp	(const void *s1, const void *s2, size_t count);
 
 /* string related functions */
 
-size_t	 td_strlen		(const char *s);
-size_t	 td_strnlen		(const char *s, size_t maxlen);
+size_t	td_strlen		(const char *s);
+size_t	td_strnlen		(const char *s, size_t maxlen);
 char	*td_strdup		(const char *s);
 char	*td_strndup		(const char *s, size_t n);
-char 	*td_stpcpy		(char *dst, const char *src);
-char 	*td_stpncpy		(char *dst, const char *src, size_t len);
-char 	*td_strcpy		(char *dst, const char *src);
+char	*td_stpcpy		(char *dst, const char *src);
+char	*td_stpncpy		(char *dst, const char *src, size_t len);
+char	*td_strcpy		(char *dst, const char *src);
 char	*td_strncpy		(char *dst, const char *src, size_t len);
-char 	*td_strcat		(char *s1, const char *s2);
-char 	*td_strncat		(char *s1, const char *s2, size_t n);
-size_t 	 td_strlcpy		(char *dst, const char *src, size_t size);
-size_t	 td_strlcat		(char *dst, const char *src, size_t size);
+char	*td_strcat		(char *s1, const char *s2);
+char	*td_strncat		(char *s1, const char *s2, size_t n);
+size_t	td_strlcpy		(char *dst, const char *src, size_t size);
+size_t	td_strlcat		(char *dst, const char *src, size_t size);
 char	*td_strchr		(const char *s, int c);
 char	*td_strrchr		(const char *s, int c);
 char	*td_strstr		(const char *big, const char *little);
 char	*td_strcasestr	(const char *big, const char *little);
 char	*td_strnstr		(const char *big, const char *little, size_t len);
-int		 td_strcmp		(const char *s1, const char *s2);
-int		 td_strncmp		(const char *s1, const char *s2, size_t n);
+int		td_strcmp		(const char *s1, const char *s2);
+int		td_strncmp		(const char *s1, const char *s2, size_t n);
 
 #endif /* _SRC_TDLIB_H_ */
